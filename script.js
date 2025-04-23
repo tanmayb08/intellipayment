ec8670ca0dea52294b1a6cd3db9219a10a028b06
Tanmay Bhosale <bhosaletanmay4@gmail.com>
2025-04-24 01:21:02 +0530

    Update script.js
    v2.1 update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let countdown = 7; // Start from 5 seconds
+
 document.getElementById('payment-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
@@ -7,7 +10,7 @@ document.getElementById('payment-form').addEventListener('submit', function(even
     // Show processing animation
     document.getElementById('processing').classList.remove('hidden');
     document.getElementById('result').classList.add('hidden');
-
+    document.getElementById('payment-form').classList.add('hidden');
     // Simulate payment processing
     setTimeout(function() {
         // Hide processing animation
@@ -24,4 +27,24 @@ document.getElementById('payment-form').addEventListener('submit', function(even
         const tickAnimation = document.querySelector('.tick-animation');
         tickAnimation.classList.remove('hidden');
     }, 3000); // Simulate a 3-second processing time
+
+    startCountdown();
+
 });
+
+
+
+    function startCountdown() {
+      const countdownElement = document.getElementById("countdown");
+
+      const interval = setInterval(() => {
+        countdownElement.textContent = countdown;
+        countdown--;
+
+        if (countdown < 0) {
+          clearInterval(interval);
+          // Redirect to another page
+          location.reload(); 
+        }
+      }, 1000);
+    }
